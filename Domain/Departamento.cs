1a7c0786949f7a9ead309b8c14ea80117e65b190
JohandryViles <johalop858@gmail.com>
2026-06-23 15:52:43 -0500

    Agregamos Entidad Departamento en el proyecto Domain
    Los cambios incluyen:
    
    Creación del archivo Departamento.cs.
    Definición de la clase Departamento con una propiedad Id.
    Este commit marca el inicio de la definición de las entidades del dominio, un paso clave para modelar la lógica de negocio de la aplicación.

---
+++ Domain/Departamento.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain;
+
+public class Departamento 
+{
+    public int Id { get; set; }
+
+    // public Dinero Precio { get; set; } = new Dinero(0, Moneda.USD);
+}
--- PracticaServers.AppHost/AppHost.cs
+++ PracticaServers.AppHost/AppHost.cs
@@ -1,3 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var dbServer = builder.AddPostgres("dbserver").WithLifetime(ContainerLifetime.Persistent);
+dbServer.WithPgAdmin(options => 
+    options.WithHostPort(5555).WithLifetime(ContainerLifetime.Persistent));
+
+var db = dbServer.AddDatabase("abndb");
+
+builder.AddProject<Projects.Api>("Api").WithExternalHttpEndpoints().WithReference(db).WaitFor(db);
+
 builder.Build().Run();
